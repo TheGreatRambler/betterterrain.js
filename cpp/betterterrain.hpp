6f3e27c62b6b2fdc8d91578884a43f532f201499
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-09-25 15:06:11 -0400

    Update betterterrain.hpp

--- cpp/betterterrain.hpp
+++ cpp/betterterrain.hpp
@@ -4,18 +4,34 @@
 #include "algorithms/random.hpp"
 
 class betterterrain {
+	private:
+		std::unordered_map myOptions; // the default options merged with the user passed options
+		// create 'array' of hash maps to store the terrain data
+		// 5 === number of elements in each array in vector
+		// auto === type of element in each array in vector
+		// std::array === type of element in the vector
+		std::vector<std::array<auto, 5>> terrainData;
+	
+		void setValue(int x, int y) {
+			
+		}
+	
+		void setValue(int x, int y) {
+			
+		}
 	public:
-	betterterrain(std::unordered_map options) {
-		using RandInstance = effolkronium::random_local;
-		RandInstance baseRand{ };
+		betterterrain(std::unordered_map options) {
+			using RandInstance = effolkronium::random_local; // starting rand instance
+			RandInstance baseRand{ };
 		
-		std::unordered_map<std::string, auto> defaultOptions = {
-        		{"seed", baseRand.get(0, 999999999999999999999)},
-        		{"freq", 50},
-        		{"biomes", new std::unordered_map<std::string, std::unordered_map<std::string, auto>>()},
-			{"onlyheight", false}
-    		};
+			std::unordered_map<std::string, auto> defaultOptions = {
+        			{"seed", baseRand.get(0, 999999999999999999999)},
+        			{"freq", 50},
+        			{"biomes", new std::unordered_map<std::string, std::unordered_map<std::string, auto>>()},
+				{"onlyheight", false}
+    			};
 		
-		options.insert(defaultOptions.begin(), defaultOptions.end()); // set default options and merge into normal options
-	}
+			options.insert(defaultOptions.begin(), defaultOptions.end()); // set default options and merge into normal options
+			myOptions = options; // set as the options
+		}
 }
