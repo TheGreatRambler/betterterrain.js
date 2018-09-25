a390954626dbbe082ced96378f0cf24c53601fa0
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-09-25 10:30:37 -0400

    Update betterterrain.hpp

--- cpp/betterterrain.hpp
+++ cpp/betterterrain.hpp
@@ -1,10 +1,21 @@
 #pragma once
-#include "random.hpp"
 #include "fastnoise/fastnoise.h"
+#include "algorithms/szudzik.hpp"
+#include "algorithms/random.hpp"
 
 class betterterrain {
 	public:
 	betterterrain(std::unordered_map options) {
-
+		using RandInstance = effolkronium::random_local;
+		RandInstance baseRand{ };
+		
+		std::unordered_map<std::string, auto> defaultOptions = {
+        		{"seed", baseRand.get(0, 999999999999999999999)},
+        		{"freq", 50},
+        		{"biomes", new std::unordered_map<std::string, std::unordered_map<std::string, auto>>()},
+			{"onlyheight", false}
+    		};
+		
+		options.insert(defaultOptions.begin(), defaultOptions.end()); // set default options and merge into normal options
 	}
 }
