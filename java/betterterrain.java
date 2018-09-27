110d7422a1de0d2e7558e8ab697de3521e4d1285
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-09-27 14:55:44 -0400

    Update betterterrain.java

--- java/betterterrain.java
+++ java/betterterrain.java
@@ -1,9 +1,17 @@
 import java.util.HashMap;
+import java.util.Random;
 
 import fastnoise.FastNoise;
 
-public class betterterrain {
-  public betterterrain(HashMap options) {
+public class Betterterrain {
+  private Random defaultRandom = new Random(); 
+  private long seed = this.defaultRandom.nextLong();
+  private Map<String, Object> biomes;
+  private bool onlyheight = false;
+  private int chunkSize = 32;
+  private String biomemap[]; // similar to a multi-dimensional array, but faster
+  
+  public Betterterrain() {
     
   }
 }
